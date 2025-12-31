78d10e2f22181d3b150609e38288dafa67a88b12
ronjomcsoman <ronjo.mcsoman@gmail.com>
2025-12-31 09:56:17 +0400

    fix: add material-icons-extended for missing icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
     
     // Accompanist for permissions
